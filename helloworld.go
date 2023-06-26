b99b538016ef0181db4d99ccab2bbf4fa5f07f04
olwu <olwu@spreadgroup.com>
2023-06-26 18:37:00 +0200

    commit 1st session'

---
+++ helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main()  {
+    fmt.Println("Hello World!")	
+	fmt.Println("The time is:", time.Now())
+	fmt.Println("My Fav no is ", rand.Intn(10))
+}
