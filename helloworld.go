b24778e222a31bd33981a1b6d835662c1a65ac56
Ashish Singh <ashishsingh21798@gmail.com>
2023-06-26 21:24:13 +0530

    hello world in go

---
+++ helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("The time is:", time.Now())
+	fmt.Println("My Fav no is ", rand.Intn(10))
+}
