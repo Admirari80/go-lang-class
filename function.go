a8e9b6aee99f72698652aa535ab8f75524dd84a7
Ashish Singh <ashishsingh21798@gmail.com>
2023-06-26 21:48:04 +0530

    function in go

---
+++ function.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func operation(x, y int) (int, int, int) {
+	return x + y, x * y, x - y
+}
+
+func main() {
+	fmt.Println(add(23, 56))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(operation(10, 20))
+}
