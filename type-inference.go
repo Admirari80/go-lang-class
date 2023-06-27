1c415e2b67f3b0db667032784bb5ffa9f2f95624
Ashish Singh <ashishsingh21798@gmail.com>
2023-06-27 21:49:30 +0530

    named return function & variable in go lang

---
+++ type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := "no"
+	fmt.Printf("v is of type %T\n", v)
+}
---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+// bool zero value false
+// int zero value 0
+//
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
