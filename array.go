3206f3f31002a8ed8460e5ef1fd2b4f56348185d
Ashish Singh <ashishsingh21798@gmail.com>
2023-06-30 21:59:34 +0530

    go stuct, array, slice

---
+++ array.go
@@ -0,0 +1,18 @@
+package main
+
+// var arr [10]int
+
+import "fmt"
+
+func main() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+	fmt.Printf("%T", primes)
+}
