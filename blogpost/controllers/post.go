070312e52f73b2b5c1084401819c3b6cf842ee6d
Ashish Singh <ashishsingh21798@gmail.com>
2023-07-12 20:47:38 +0530

    Blogpost project complete

--- blogpost/controllers/post.go
+++ blogpost/controllers/post.go
@@ -23,3 +23,54 @@ func CreatePost(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": post})
 }
+
+func FindPosts(c *gin.Context) {
+	var posts []models.Blogpost
+	models.DB.Find(&posts)
+
+	c.JSON(http.StatusOK, gin.H{"data": posts})
+}
+
+func FindPost(c *gin.Context) {
+	var post models.Blogpost
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&post).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": post})
+}
+
+type UpdatePostInput struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+func UpdatePost(c *gin.Context) {
+	var post models.Blogpost
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&post).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	var input UpdatePostInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatePost := models.Blogpost{Title: input.Title, Content: input.Content}
+	models.DB.Model(&post).Updates(&updatePost)
+	c.JSON(http.StatusOK, gin.H{"data": post})
+}
+
+func DeletePost(c *gin.Context) {
+	var post models.Blogpost
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&post).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Delete(&post)
+	c.JSON(http.StatusOK, gin.H{"data": "Success"})
+}
